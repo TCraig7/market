aded234823d71f64262542399c3459ae9452c80a
Tara Craig <tara.craig7@gmail.com>
2018-07-31 09:22:57 -0600

    Add market class and test file. Add setup method to test file and test it exists. Initialize method and attr_reader added to class file. All tests passing.

---
+++ test/market_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+require './lib/market'
+
+class MarketTest < Minitest::Test
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
+
+end
